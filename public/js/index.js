0a7205e98b98d08adac7330ff9aeb436a4df44f2
Ryan Gill <ryanjgill2@gmail.com>
2018-01-02 20:41:43 -0600

    button

--- public/js/index.js
+++ public/js/index.js
@@ -37,15 +37,6 @@ new Vue({
     clearBarcodes() {
       this.currentBarcode = "Awaiting Scan..."
       this.scannedBarcodes = []
-    },
-    updateChart() {
-      let series = this.$refs.highcharts.chart.series
-      let ticks = new Date().getTime()
-			series[0].addPoint([ticks, +this.$data.room_temp], false, true)
-			series[1].addPoint([ticks, +this.$data.case_temp], false, true)
-			series[2].addPoint([ticks, +this.$data.radiator_temp], false, true)
-			series[3].addPoint([ticks, +this.$data.gpu_1_temp], false, true)
-			series[4].addPoint([ticks, +this.$data.gpu_2_temp], true, true)
     }
   }
 });
